25ace0e197109936261252fb12bcef363419b688
MuhammadBurhan235 <muhammadburhan@student.telkomuniversity.ac.id>
2024-08-09 17:41:23 +0700

    update landing page Al-Fath

--- src/pages/Landing/LandingAlFath.tsx
+++ src/pages/Landing/LandingAlFath.tsx
@@ -10,8 +10,46 @@ import {
   faLessThan,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../App.css";
+import { useEffect, useState } from "react";
+
+interface Slide {
+  color: string;
+  width: string;
+}
 
 export function LandingAlFath() {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const slides: Slide[] = [
+    { color: "gray", width: "215px" },
+    { color: "red", width: "215px" },
+    { color: "gray", width: "215px" },
+    { color: "black", width: "215px" },
+    { color: "yellow", width: "215px" },
+    { color: "black", width: "215px" },
+  ];
+
+  const handleNodeClick = (index: number) => {
+    const newIndex = index * 3; // Multiply the index by 3 to shift by three slides
+    // Ensure the new index does not exceed the slides length
+    setCurrentIndex(Math.min(newIndex, slides.length - 3));
+  };
+
+  // Calculate the visible slides based on the current index
+  const visibleSlides = slides.slice(currentIndex, currentIndex + 3);
+
+  // Automatically move to the next slide every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => {
+        const nextIndex = prevIndex + 3;
+        return nextIndex < slides.length ? nextIndex : 0; // Reset to 0 if exceeding
+      });
+    }, 5000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
   return (
     <div>
       <div className="area">
@@ -140,36 +178,41 @@ export function LandingAlFath() {
                     <source src={triDiVideoWebM} type="video/webm" />
                     Your browser does not support the video tag.
                   </video>
-                  <div
-                    className="cardList2"
-                    style={{
-                      width: "210px",
-                      padding: "6.5px",
-                      backgroundColor: "#c04545",
-                      color: "white",
-                      fontSize: "12px",
-                    }}
-                  >
-                    <div
-                      className="cardContainer"
+                  <a href="http://localhost:5173/portofolio-burhan/tridialfath">
+                    <button
+                      className="button1"
                       style={{
-                        gap: "0px",
+                        width: "210px",
+                        padding: "9px",
+                        backgroundColor: "#c04545",
+                        color: "white",
+                        fontSize: "12px",
+                        textTransform: "unset",
+                        borderRadius: "15px",
                       }}
                     >
-                      <h4 style={{ width: "200px" }}>
-                        <FontAwesomeIcon icon={faGreaterThan} /> Interaction
-                        Picture <FontAwesomeIcon icon={faLessThan} />
-                      </h4>
-                      <div className="cardContainer">
-                        <p>
-                          Dekstop <FontAwesomeIcon icon={faCheck} />
-                        </p>
-                        <p>
-                          Mobile <FontAwesomeIcon icon={faCheck} />
-                        </p>
+                      <div
+                        className="cardContainer"
+                        style={{
+                          gap: "0px",
+                          fontWeight: "normal",
+                        }}
+                      >
+                        <h4 style={{ width: "200px" }}>
+                          <FontAwesomeIcon icon={faGreaterThan} /> Interaction
+                          Picture <FontAwesomeIcon icon={faLessThan} />
+                        </h4>
+                        <div className="cardContainer">
+                          <p>
+                            Dekstop <FontAwesomeIcon icon={faCheck} />
+                          </p>
+                          <p>
+                            Mobile <FontAwesomeIcon icon={faCheck} />
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  </div>
+                    </button>
+                  </a>
                 </div>
               </div>
               <div
@@ -192,20 +235,23 @@ export function LandingAlFath() {
                     <source src={intPicVideoWebM} type="video/webm" />
                     Your browser does not support the video tag.
                   </video>
-                  <div
-                    className="cardList2"
+                  <button
+                    className="button1"
                     style={{
                       width: "210px",
-                      padding: "6.5px",
+                      padding: "9px",
                       backgroundColor: "#c04545",
                       color: "white",
                       fontSize: "12px",
+                      textTransform: "unset",
+                      borderRadius: "15px",
                     }}
                   >
                     <div
                       className="cardContainer"
                       style={{
                         gap: "0px",
+                        fontWeight: "normal",
                       }}
                     >
                       <h4 style={{ width: "200px" }}>
@@ -221,7 +267,7 @@ export function LandingAlFath() {
                         </p>
                       </div>
                     </div>
-                  </div>
+                  </button>
                 </div>
               </div>
               <div
@@ -284,29 +330,37 @@ export function LandingAlFath() {
                       style={{
                         width: "115px",
                         padding: "0px",
-                        paddingTop: "6.5px",
+                        paddingTop: "4.5px",
                       }}
                     >
-                      <img
-                        src={images["YT Logo"]}
-                        alt=""
-                        style={{ width: "35px", height: "auto" }}
-                      />
-                      <img
-                        src={images["FB Logo"]}
-                        alt=""
-                        style={{ width: "35px", height: "auto" }}
-                      />
-                      <img
-                        src={images["IG Logo"]}
-                        alt=""
-                        style={{ width: "35px", height: "auto" }}
-                      />
-                      <img
-                        src={images["LINE Logo"]}
-                        alt=""
-                        style={{ width: "35px", height: "auto" }}
-                      />
+                      <a href="https://youtube.com/@al-fathuniversitastelkom8769?si=VJqU37TKGnLIQdJe">
+                        <img
+                          src={images["YT Logo"]}
+                          alt=""
+                          style={{ width: "35px", height: "auto" }}
+                        />
+                      </a>
+                      <a href="https://www.facebook.com/ALFATHUniversitasTelkom">
+                        <img
+                          src={images["FB Logo"]}
+                          alt=""
+                          style={{ width: "35px", height: "auto" }}
+                        />
+                      </a>
+                      <a href="https://www.instagram.com/alfathtelu">
+                        <img
+                          src={images["IG Logo"]}
+                          alt=""
+                          style={{ width: "35px", height: "auto" }}
+                        />
+                      </a>
+                      <a href="">
+                        <img
+                          src={images["LINE Logo"]}
+                          alt=""
+                          style={{ width: "35px", height: "auto" }}
+                        />
+                      </a>
                     </div>
                   </div>
                   <div
@@ -368,13 +422,68 @@ export function LandingAlFath() {
                     </p>
                   </div>
                   <div
-                    className="cardContainer"
+                    className="slideContainer"
                     style={{
-                      width: "650px",
-                      borderTopLeftRadius: "0px",
-                      borderBottomLeftRadius: "0px",
+                      width: "648px",
+                      height: "128.5px",
+                      borderTopRightRadius: "15px",
+                      borderBottomRightRadius: "15px",
+                      padding: "0px",
+                      gap: "0px",
+                      overflow: "hidden",
+                      display: "flex",
+                      position: "relative",
                     }}
-                  ></div>
+                  >
+                    {visibleSlides.map((slide, index) => (
+                      <div
+                        key={index}
+                        className="slideItem"
+                        style={{
+                          backgroundColor: slide.color,
+                          width: slide.width,
+                        }}
+                      ></div>
+                    ))}
+
+                    {/* Navigation nodes */}
+                    <div
+                      style={{
+                        padding: "5px",
+                        borderRadius: "5px",
+                        position: "absolute",
+                        marginTop: "3px",
+                        backgroundColor: "rgba(255, 255, 255, 0.150)",
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                        }}
+                      >
+                        {slides
+                          .slice(0, Math.ceil(slides.length / 3))
+                          .map((_, index) => (
+                            <div
+                              key={index}
+                              onClick={() => handleNodeClick(index)}
+                              style={{
+                                width: "10px",
+                                height: "10px",
+                                backgroundColor:
+                                  currentIndex === index * 3
+                                    ? "#c04545"
+                                    : "lightgray", // Change color here
+                                borderRadius: "50%",
+                                margin: "0 5px",
+                                cursor: "pointer",
+                              }}
+                            />
+                          ))}
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
